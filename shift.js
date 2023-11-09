0c22a2b4cb377f7e939007ca8a2339ab04668fa2
eunjeong <eunjeong990731@gmail.com>
2023-11-09 11:09:14 +0900

    shift 메서드사용 후 출력

--- shift.js
+++ shift.js
@@ -5,3 +5,9 @@ const arry = [1, 2, 3, 4, 5];
 //* shift() 메서드를 사용 할 시 배열의 첫번쨰 요소 제거
 
 const cut = arry.shift();
+
+//* arry 출력시 첫번째 요소가 제거된 다음 출력이 됨
+//* cut 출력시 제거된 요소 확인이 가능하다.
+
+console.log(arry);
+console.log(cut);
