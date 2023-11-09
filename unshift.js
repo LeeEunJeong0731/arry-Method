5b3475f510e1e07d38143306d90e5e5fb9c8ef82
eunjeong <eunjeong990731@gmail.com>
2023-11-09 11:14:15 +0900

    unshift 사용후 출력

--- unshift.js
+++ unshift.js
@@ -4,3 +4,9 @@ const array = [2, 3, 4, 5];
 
 //* unshift() 메서드를 사용하여 배열의 시작에 요소 추가
 const puls = array.unshift(1);
+
+//* arry 출력시 추가된 요소까지 출력
+//* puls 출력시 배열의 새로운 길이가 출력된다. 5가 뜬다.
+
+console.log(array);
+console.log(puls);
