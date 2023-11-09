c915551bac6ea6c48e9653bd917761a0ee0d09e6
eunjeong <eunjeong990731@gmail.com>
2023-11-09 11:03:33 +0900

    pop 메서드 사용 후 출력

--- pop.js
+++ pop.js
@@ -2,3 +2,8 @@ const arry = [1, 2, 3, 4, 5];
 //* pop() 메서드를 사용할 시 배열의 마지막 요소를 삭제한다.
 
 const last = arry.pop();
+
+//* arry 출력 시 맨끝 요소는 지워져서 출력 됨
+//* last 출력 시 제거된 요소를 보여줌
+console.log(arry);
+console.log(last);
