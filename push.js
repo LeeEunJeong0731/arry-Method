cf19b47085bbb2c9c232ac2901a84f179994729f
eunjeong <eunjeong990731@gmail.com>
2023-11-09 10:57:32 +0900

    출력 확인과 주석처리

--- push.js
+++ push.js
@@ -2,3 +2,9 @@ const arry = [1, 2, 3, 4, 5];
 
 //* push는 배열의 끝에 하나 이상의 요소를 추가하고 배열의 새 길이를 반환한다.
 const puls = arry.push(6, 7);
+
+//* console.log (arry) 를 하면 1~7 까지 나온다.
+//* console.log (puls) 새로운 배열의 끝자리인 7이 나온다.
+
+console.log(arry);
+console.log(puls);
